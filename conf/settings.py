dbc461806c16b0e1b26b804cb87c411575ee7206
isaacbarcroft <i_barcroft@yahoo.com>
2021-10-25 09:14:21 -0400

    procfile

--- conf/settings.py
+++ conf/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '127.0.0.1', 'https://django-react-news-article.herokuapp.com/']
+ALLOWED_HOSTS = [ '127.0.0.1',
+ 'https://django-react-news-article.herokuapp.com/']
 
 
 # Application definition
