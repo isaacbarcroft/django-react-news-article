5b40f9b8da0e66dc6ce6b070067b5417ecfa5c7b
isaacbarcroft <i_barcroft@yahoo.com>
2021-10-25 09:45:12 -0400

    index view

--- conf/urls.py
+++ conf/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('', include('frontend.urls', namespace='frontend')),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
